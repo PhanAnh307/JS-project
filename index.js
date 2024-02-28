f15e9f51015220fea2e481925970f5b1a4b71c9e
HongAnh <lotus.honganhp@gmail.com>
2024-02-28 22:11:41 +0700

    rewrite index.js to learn and clean code

--- index.js
+++ index.js
@@ -6,56 +6,48 @@ var partytime;
 var evening = 18;
 
 // Getting it to show the current time on the page
-var showCurrentTime = function()
-{
-    // display the string on the webpage
-    var clock = document.getElementById('clock');
- 
-    var currentTime = new Date();
- 
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    var meridian = "AM";
- 
-    // Set hours
-    if (hours >= noon)
-    {
-      meridian = "PM";
-    }
+var showCurrentTime = function() {
+  //display the string on the webpage
+  var clock = document.getElementById('clock');
+  var currentTime = new Date();
 
-    if (hours > noon)
-    {
-      hours = hours - 12;
-    }
- 
-    // Set Minutes
-    if (minutes < 10)
-    {
-        minutes = "0" + minutes;
-    }
- 
-    // Set Seconds
-    if (seconds < 10)
-    {
-        seconds = "0" + seconds;
-    }
- 
-    // put together the string that displays the time
-    var clockTime = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
- 
-    clock.innerText = clockTime;
-};
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var meridian = "AM";
 
+  //set hours
+  if(hours >= noon) {
+    meridian = "PM";  
+  }
+  if(hours > noon) {
+    hours = hours - 12;
+  }
+  //set minutes
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  //set seconds
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  //pt together the string that displays the time
+  var timeclock = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+
+  clock.innerText = timeclock;
+}
 // Getting the clock to increment on its own and change out messages and pictures
 var updateClock = function() 
-{
+{ 
   var time = new Date().getHours();
-  var messageText;
   var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+  var messageText;
 
-  var timeEventJS = document.getElementById("timeEvent");
-  var lolcatImageJS = document.getElementById('lolcatImage');
+  var event = document.getElementById("timeEvent");
+  var getImageJs = document.getElementById("lolcatImage");
+  
   
   if (time == partytime)
   {
@@ -93,10 +85,10 @@ var updateClock = function()
     messageText = "Good afternoon!";
   }
 
-  console.log(messageText); 
-  timeEventJS.innerText = messageText;
-  lolcatImage.src = image;
+  event.innerText = messageText;
+  getImageJs.src = image;
   
+
   showCurrentTime();
 };
 updateClock();
