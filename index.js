2241fecb206ae342330475a1674d2a61bd276714
HongAnh <lotus.honganhp@gmail.com>
2024-02-29 16:38:06 +0700

    delete all the index.js and write again to clean code

--- index.js
+++ index.js
@@ -1,154 +1,132 @@
-var wakeuptime = 7;
 var noon = 12;
-var lunchtime = 12;
-var naptime = lunchtime + 2;
 var partytime;
 var evening = 18;
+var night = 22;
 
-// Getting it to show the current time on the page
+// Make the clock work
 var showCurrentTime = function() {
-  //display the string on the webpage
   var clock = document.getElementById('clock');
-  var currentTime = new Date();
-
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
   var meridian = "AM";
+  var hours = new Date().getHours();
+  var minutes = new Date().getMinutes();  
+  var seconds = new Date().getSeconds();
 
-  //set hours
+  // set meridian 
   if(hours >= noon) {
-    meridian = "PM";  
+    meridian = "PM";
   }
-  if(hours > noon) {
+  // set hours
+  if(hours > 12) {
     hours = hours - 12;
   }
-  //set minutes
-  if (minutes < 10) {
+
+  // set minutes
+  if(minutes < 10) {
     minutes = "0" + minutes;
   }
 
   //set seconds
-  if (seconds < 10) {
-    seconds = "0" + seconds;
+  if(seconds < 10) {  
+    seconds = "0" + seconds;  
   }
 
-  //pt together the string that displays the time
-  var timeclock = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
 
-  clock.innerText = timeclock;
+
+  
+ // formed the clock display form
+  clock.innerText = hours + ':' + minutes + ':' + seconds + " " + meridian + "!";
 }
-// Getting the clock to increment on its own and change out messages and pictures
-var updateClock = function() 
-{ 
-  var time = new Date().getHours();
-  var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-  var messageText;
 
-  var event = document.getElementById("timeEvent");
-  var getImageJs = document.getElementById("lolcatImage");
-  
-  
-  if (time == partytime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
-    messageText = "Let's party!";
-  }
-  else if (time == wakeuptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
-    messageText = "Wake up!";
-  }
-  else if (time == lunchtime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
-    messageText = "Let's have some lunch!";
-  }
-  else if (time == naptime)
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
-    messageText = "Sleep tight!";
-  }
-  else if (time < noon)
-  {
-    image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg";
-    messageText = "Good morning!";
-  }
-  else if (time >= evening)
-  {
-    image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
-    messageText = "Good evening!";
-  }
-  else
-  {
-    image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
-    messageText = "Good afternoon!";
-  }
+ showCurrentTime();
+ setInterval(showCurrentTime, 1000);
 
-  event.innerText = messageText;
-  getImageJs.src = image;
-  
+// Active picture and reported message text to display
+var updateClock = function() {
+    var time = new Date().getHours(); 
+    var message;
+    var eventReportJS = document.getElementById('timeEvent'); 
+    var lolCatImage = document.getElementById('lolcatImage');
+    var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+    
 
-  showCurrentTime();
-};
-updateClock();
+    if(time == partytime) {
+      message = "Let's party!"
+      image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/partyTime.jpg";
+    }
+    else if (time == wakeuptime) {
+      message = "Wake Up Time!"
+      image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat1.jpg";
+    }else if (time == lunchtime) {
+      message = "Let's have some lunch!";
+      image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat2.jpg";
+    }else if (time == naptime) { 
+      message = "Sleep tight!";
+      image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/cat3.jpg";
+    }else if (time < noon) { 
+      message = "Good Morning!";  
+      image = "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg"; 
 
-// Getting the clock to increment once a second
-var oneSecond = 1000;
-setInterval( updateClock, oneSecond);
+    }else if (time >= evening){
+      message = "Good evening!";
+      image = "https://upload.wikimedia.org/wikipedia/commons/8/8c/Cat_sleep.jpg";
+    }else {
+      message = "Good afternoon!";
+      image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/normalTime.jpg";
+    }
+    console.log(message);
+    console.log(image);
+    eventReportJS.innerText = message;
+    lolCatImage.src = image;
+    showCurrentTime();
+    
+}
+setInterval(updateClock, 1000);
 
-
-// Getting the Party Time Button To Work
+// Active party button 
 var partyButton = document.getElementById("partyTimeButton");
 
 var partyEvent = function()
-{
-    if (partytime < 0) 
-    {
-        partytime = new Date().getHours();
-        partyTimeButton.innerText = "Party Over!";
-        partyTimeButton.style.backgroundColor = "#0A8DAB";
-    }
-    else
-    {
-        partytime = -1;
-        partyTimeButton.innerText = "Party Time!";
-        partyTimeButton.style.backgroundColor = "#222";
-    }
+ {
+  
+  if (partytime < 0)
+   {
+    partytime = new Date().getHours();
+    partyButton.innerText = "Party Over!";
+    partyButton.style.backgroundColor = "#222"
+    
+  }
+  else 
+  {
+    partytime = -1;
+    partyButton.innerText = "Party Time!" ;
+    partyButton.style.backgroundColor = "#00000";
+  }
 };
-
 partyButton.addEventListener("click", partyEvent);
-partyEvent(); 
+partyEvent();
 
+// Active wakeup selector 
+var wakeUpSelector = document.getElementById('wakeUpTimeSelector'); 
 
-// Activates Wake-Up selector
-var wakeUpTimeSelector =  document.getElementById("wakeUpTimeSelector");
-
-var wakeUpEvent = function()
-{
-    wakeuptime = wakeUpTimeSelector.value;
+var wakeUpEvent = function() {
+  wakeuptime = wakeUpSelector.value;
 };
+wakeUpSelector.addEventListener('change', wakeUpEvent);
+wakeUpEvent();
+// Active lunch time selector
+var lunchTimeSelector = document.getElementById('lunchTimeSelector'); 
 
-wakeUpTimeSelector.addEventListener("change", wakeUpEvent);
-
-
-// Activates Lunch selector
-var lunchTimeSelector =  document.getElementById("lunchTimeSelector");
-
-var lunchEvent = function()
-{
-    lunchtime = lunchTimeSelector.value;
+var lunchTimeEvent = function() {
+  lunchtime = lunchTimeSelector.value;
 };
+lunchTimeSelector.addEventListener('change', lunchTimeEvent); 
+lunchTimeEvent(); 
 
-lunchTimeSelector.addEventListener("change", lunchEvent);
+// Active nap time selector
+var napTimeSelector = document.getElementById('napTimeSelector');
 
-
-// Activates Nap-Time selector
-var napTimeSelector =  document.getElementById("napTimeSelector");
-
-var napEvent = function()
-{
-    naptime = napTimeSelector.value;
+var napTimeEvent = function() {
+  naptime = napTimeSelector.value;
 };
-
-napTimeSelector.addEventListener("change", napEvent);
+napTimeSelector.addEventListener('change', napTimeEvent);
+napTimeEvent();
